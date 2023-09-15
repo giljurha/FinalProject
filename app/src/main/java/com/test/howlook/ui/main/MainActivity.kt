34acb7316f5a2f775b78d039b90a4981d2e16c92
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2023-09-15 19:48:36 +0900

    메인액티비티 분기 구현

--- app/src/main/java/com/test/howlook/ui/main/MainActivity.kt
+++ app/src/main/java/com/test/howlook/ui/main/MainActivity.kt
@@ -2,11 +2,40 @@ package com.test.howlook.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.test.howlook.R
+import com.test.howlook.databinding.ActivityMainBinding
+import com.test.howlook.ui.community.CommunityFragment
+import com.test.howlook.ui.myinfo.MyInfoFragment
+import com.test.howlook.ui.upload.UploadFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        replaceFragment(UploadFragment())
+
+        activityMainBinding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(UploadFragment())
+                R.id.profile -> replaceFragment(MyInfoFragment())
+                R.id.community -> replaceFragment(CommunityFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainContainer,fragment)
+        fragmentTransaction.commit()
     }
 }
